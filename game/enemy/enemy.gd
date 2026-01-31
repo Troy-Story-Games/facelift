e319cb65bf1613fdecf5ab368563b7e4f0d9d6fc
Sean LaPlante <laplante.sean@gmail.com>
2026-01-31 08:26:15 -0500

    Stuff

--- game/enemy/enemy.gd
+++ game/enemy/enemy.gd
@@ -5,18 +5,23 @@ const PoofParticles = preload("res://game/fx/poof_particles.tscn")
 
 @export var health: float = 100.0
 @export var speed: float = 1.0
+@export var arrival_safe_distance: float = 0.1
 
 var direction: Vector3 = Vector3.ZERO
 var ragdoll: bool = false
 var target_pos: Vector3 = Vector3.ZERO
 var target_set: bool = false
 var velocity: Vector3
+var physical_bone_simulator_3d: PhysicalBoneSimulator3D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var enemy_model: Node3D = $EnemyModel
+@onready var collision_shape_3d: CollisionShape3D = $CollisionShape3D
+@onready var enemy: Node3D = $Enemy
+@onready var construction_enemy: Node3D = $ConstructionEnemy
 
 func _ready():
     freeze = true
+    physical_bone_simulator_3d = construction_enemy.find_child("PhysicalBoneSimulator3D") as PhysicalBoneSimulator3D
 
 func set_target(target_transform: Transform3D):
     target_pos = target_transform.origin
@@ -31,6 +36,8 @@ func _process(_delta):
 func _physics_process(delta):
     if ragdoll or not target_set:
         return  # Don't need to move if we're ragdoll or we don't have a target
+    if global_transform.origin.distance_to(target_pos) <= arrival_safe_distance:
+        return  # We have arrived (TODO: Attack? Build?)
 
     direction = global_transform.origin.direction_to(target_pos).normalized()
     direction.y = 0  # Only need the x,z direction
@@ -38,13 +45,8 @@ func _physics_process(delta):
     look_at(global_transform.origin - direction, Vector3.UP)
     move_and_collide(velocity, false)
 
-func _on_grabbed(pickable: Variant, by: Variant) -> void:
-    print("Grabbed by ", by)
+func _on_grabbed(_pickable: Variant, _by: Variant) -> void:
     ragdoll = true
-    var skel = enemy_model.find_child("Skeleton3D") as Skeleton3D
-    skel.physical_bones_start_simulation()
 
-func _on_dropped(pickable: Variant) -> void:
+func _on_dropped(_pickable: Variant) -> void:
     print("Dropped")
-    #set_deferred("freeze", false)
-    #apply_central_impulse(Vector3.UP * 0.1)
